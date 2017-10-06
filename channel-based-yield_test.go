f1d92ae368bce72327b0fdf3a3e97ced286582be
Chris Tomich <chris.tomich@gmail.com>
2017-10-06 12:39:44 +0800

    A pattern that emulates a C# yield in Go

---
+++ channel-based-yield_test.go
@@ -0,0 +1,41 @@
+package channelbasedyield
+
+import (
+	"testing"
+)
+
+func iterateItemsWithYield() chan int {
+	c := make(chan int)
+
+	go (func() {
+		for i := 0; i < 1000; i++ {
+			c <- i
+		}
+		close(c)
+	})()
+
+	return c
+}
+
+// BenchmarkChannelYield benchmarks the pattern for using a channel like a C# yield.
+func BenchmarkChannelYield(b *testing.B) {
+	c := iterateItemsWithYield()
+	for i := 0; i < b.N; i++ {
+		sum := 0
+
+		for myNum := range c {
+			sum += myNum
+		}
+	}
+}
+
+// BenchmarkStandardLoop is just a standard loop.
+func BenchmarkStandardLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sum := 0
+
+		for i := 0; i < 1000; i++ {
+			sum += i
+		}
+	}
+}
